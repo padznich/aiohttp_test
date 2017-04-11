54a2edd010d8fdf67243b4a808673fe97d970d18
padznich <slabkopg@gmail.com>
2017-04-11 17:52:38 +0300

    [ADD] test for questions/choices views

--- polls/libs/logger.py
+++ polls/libs/logger.py
@@ -4,7 +4,10 @@ import logging
 log = logging.getLogger("app")
 log.setLevel(logging.DEBUG)
 
-f = logging.Formatter("[L:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s", datefmt="%d-%m-%Y %H:%M:%S")
+f = logging.Formatter(
+    "[L:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s",
+    datefmt="%d-%m-%Y %H:%M:%S"
+)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(f)
